8338854f4d58dde89c2739e79cd07e26b78a69a3
TheShinyBunny <shinybunny123@gmail.com>
2022-02-02 00:48:06 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<template>
+  <main>
+    
+  </main>
+</template>
